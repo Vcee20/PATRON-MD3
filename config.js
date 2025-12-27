3cc303ce5138f90d9418998a73d1f3cd007973fc
Vcee20 <heisvcee06@gmail.com>
2025-12-27 14:19:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ require('dotenv').config();
 //================= { SETTINGS } =================\\
 global.prefix = (process.env.PREFIX || ".").trim().split(/\s+/);
 // your desired prefix symbol only
-global.owner = process.env.OWNER_NUMBER || "2348133729715"; // owner number , for multiple number
+global.owner = process.env.OWNER_NUMBER || "2348138812337"; // owner number , for multiple number
 global.sudo = process.env.SUDO  || "";
-global.ownername = process.env.OWNER_NAME || "Patron TechX"; //set bot owner name here 
+global.ownername = process.env.OWNER_NAME || "Godfather"; //set bot owner name here 
 global.botname = process.env.BOT_NAME || "ᴘᴀᴛʀᴏɴ-ᴍᴅ"
 global.author = process.env.AUTHOR  || "Patron TechX";
 global.packname = process.env.PACK_NAME  || "© ᴘᴀᴛʀᴏɴ ᴍᴅ ²⁵";
-global.session = process.env.SESSION_ID || ""
+global.session = process.env.SESSION_ID || "PATRON-MD~5ee9ae44c5d13fc3fba99ae0f7a2bde9"
 global.timezone = process.env.TIME_ZONE || "Africa/Lagos";
 global.simbol = process.env.SYMBOL || '🚹';
 global.thumb = process.env.THUMB || "https://files.catbox.moe/e71nan.png"
